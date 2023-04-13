4bd285a9478312304161cfcb9a8ec4323057b548
Sahil Sharma <hycamtinn@gmail.com>
2023-04-13 08:33:01 +0530

    Initialize express app and add simple root route

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("<h2>Hi There &#9995</h2>");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
